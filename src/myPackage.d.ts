97fd8ef772348b3fc9661427203b72a9184bb581
foresec <foresec@naver.com>
2024-01-10 17:50:19 +0900

    0110-3

--- src/myPackage.d.ts
+++ src/myPackage.d.ts
@@ -1,9 +1,10 @@
 // ts가 패키지(이 경우, js로 작성된 myPackage)를 인식할 수 있도록 모듈 선언
 // 타입 지정
-interface Config {
-  url: string;
-}
+
 declare module "myPackage" {
+	interface Config {
+		url: string;
+	}
   function init(config: Config): boolean;
   function exit(code: number): number;
 }
